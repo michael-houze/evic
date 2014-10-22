67adff196857ecf912ade842f1b605dde88fbfdb
Michael Houze <michael.houze@gmail.com>
2014-10-22 15:04:06 -0400

    Create Dashboard ViewModel
    - Added dummy notifications
    - Make view model and test it

--- MVC_DATABASE/Controllers/DashboardTestController.cs
+++ MVC_DATABASE/Controllers/DashboardTestController.cs
@@ -1,8 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.Globalization;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using MVC_DATABASE.Models;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MVC_DATABASE.Models.ViewModels;
 
 namespace MVC_DATABASE.Controllers
 {
@@ -11,7 +27,9 @@ namespace MVC_DATABASE.Controllers
         // GET: DashboardTest
         public ActionResult Index()
         {
-            return View();
+            Dashboard model = new Dashboard();
+            model.unregisteredVendors = 3334444;
+            return View(model);
         }
     }
 }
---
+++ MVC_DATABASE/Models/ViewModels/Dashboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_DATABASE.Models.ViewModels
+{
+    public class Dashboard
+    {
+        public int unregisteredVendors;
+    }
+}
