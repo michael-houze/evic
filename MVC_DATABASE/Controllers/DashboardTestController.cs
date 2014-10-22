0639bade4511af925b13ead93bb469ba55d97db6
Michael Houze <michael.houze@gmail.com>
2014-10-22 11:19:06 -0400

    Add Dashboard Test Controller

---
+++ MVC_DATABASE/Controllers/DashboardTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DATABASE.Controllers
+{
+    public class DashboardTestController : Controller
+    {
+        // GET: DashboardTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
