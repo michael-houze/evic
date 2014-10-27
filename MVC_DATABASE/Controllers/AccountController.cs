490445a2ab62f33847214528ede4fdc6c6455265
bongo19 <f0bong01@louisville.edu>
2014-10-27 00:12:19 -0400

    Updated Email Confirmation
    Added "EmailNotConfirmed" view.
    
    When user tries to log on without confirming their email address, they
    are re-directed to this view instead of receiving system error message.
    
    User is no longer automatically logged in when they click on the
    confirmation link.

--- MVC_DATABASE/Controllers/AccountController.cs
+++ MVC_DATABASE/Controllers/AccountController.cs
@@ -86,6 +86,17 @@ namespace MVC_DATABASE.Controllers
                 return View(model);
             }
 
+            // Require the user to have a confirmed email before they can log on.
+            var user = await UserManager.FindByNameAsync(model.Email);
+            if (user != null)
+            {
+                if (!await UserManager.IsEmailConfirmedAsync(user.Id))
+                {
+                    //ViewBag.errorMessage = "You must confirm your email in order to log on.";
+                    return View("EmailNotConfirmed");
+                }
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
@@ -192,7 +203,7 @@ namespace MVC_DATABASE.Controllers
                     db.VENDORCONTACTs.Add(vendorcontact);
 
                     await db.SaveChangesAsync();
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                   // await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false); <<---- Not originally commented out. Commented out to prevent log in until the user is confirmed.
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
