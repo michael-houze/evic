f46b6d44ae5fa1e0e454e1b664435e234f6eaaef
ENeises10 <evan.neises@gmail.com>
2014-10-28 16:41:04 -0400

    Upload functionality from Cody (need to fix 404 error)

---
+++ MVC_DATABASE/Controllers/VendorContractController.cs
@@ -0,0 +1,52 @@
+﻿using MVC_DATABASE.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVC_DATABASE.Controllers
+{
+    public class VendorContractController : Controller
+    {
+        //
+        // GET: /VendorContract/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Respond(string Id)
+        {
+            var vresponse = new Contract { Id = Id };
+
+            return View();
+        }
+
+        //
+        //Stores the uploaded form from View VendorContract/Response
+        [HttpPost]
+        public ActionResult Respond(HttpPostedFileBase file)
+        {
+            //Verify a file is selected.
+            if (file != null)
+            {
+                //Extract the file name.
+                var fileName = Path.GetFileName(file.FileName);
+                //Establishes where to save the path using the extracted name.
+                var path = Path.Combine(Server.MapPath(@"~/Content/ContractStore/TestContracts"), fileName);
+                //Saves file.
+                file.SaveAs(path);
+            }
+            //Sends the user back to their respective RFI Index page.
+            return RedirectToAction("Index");
+        }
+
+
+        public string Details()
+        {
+            return "Details...";
+        }
+	}
+}
