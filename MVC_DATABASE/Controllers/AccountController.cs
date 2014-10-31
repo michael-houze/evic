e7b54401f560ef521ae7cd90ae35185f4a2215e9
jshields91 <jeshie02@louisville.edu>
2014-10-30 22:49:49 -0400

    REMOVED ENTITY MODEL AND DATA CONNECTION
    REMOVED ENTITY MODEL AND DATA CONNECTION

--- MVC_DATABASE/Controllers/AccountController.cs
+++ MVC_DATABASE/Controllers/AccountController.cs
@@ -189,18 +189,20 @@ namespace MVC_DATABASE.Controllers
                 var result = await UserManager.CreateAsync(user, model.RegisterViewModel.Password);
                 if (result.Succeeded)
                 {
-                    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
-                    Roles.AddUserToRole(user.Id, "Vendor");
+                    //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+                    //Roles.AddUserToRole(user.Id, "Vendor");
                     var vendor = new VENDOR { Id = user.Id, FirstName = model.VENDOR.FirstName, LastName = model.VENDOR.LastName, ORGANIZATION = model.VENDOR.ORGANIZATION };
                     var vendorcontact = new VENDORCONTACT { Id = user.Id, CONTACTNAME = model.VENDORCONTACT.CONTACTNAME, CONTACTEMAIL = model.VENDORCONTACT.CONTACTEMAIL, CONTACTPHONE = model.VENDORCONTACT.CONTACTPHONE };
                     vendor.VENDSTATUS = "PENDING";
                     vendor.SANCTIONED = false;
-                    foreach(var x in model.CategoryList)
-                    {                        
-                        var offeredCategory = new OFFEREDCATEGORY {Id = user.Id, CATEGORY = x, ACCEPTED = false};
-                        db.OFFEREDCATEGORies.Add(offeredCategory);
+                    if (model.CategoryList != null)
+                    {
+                        foreach (var x in model.CategoryList)
+                        {
+                            var offeredCategory = new OFFEREDCATEGORY { Id = user.Id, CATEGORY = x, ACCEPTED = false };
+                            db.OFFEREDCATEGORies.Add(offeredCategory);
+                        }
                     }
-                    
                     db.VENDORs.Add(vendor);
                     db.VENDORCONTACTs.Add(vendorcontact);
 
