32f0ba2304245ca0888ca8e4ea903ada1f4e17fd
Michael Houze <michael.houze@gmail.com>
2014-11-04 16:34:15 -0500

    JavaScript background colors
    - Make lighter for readability

--- MVC_DATABASE/Controllers/HomeController.cs
+++ MVC_DATABASE/Controllers/HomeController.cs
@@ -59,12 +59,12 @@ namespace MVC_DATABASE.Controllers
 
             foreach (var rfi in db.RFIs)
             {
-                events.Add(new CalendarEvent("RFI #" + rfi.RFIID.ToString() + " Expires", true, rfi.EXPIRES, "#4DB3D0"));
+                events.Add(new CalendarEvent("RFI #" + rfi.RFIID.ToString() + " Expires", true, rfi.EXPIRES, "#74C0D9"));
             }
 
             foreach (var rfp in db.RFPs)
             {
-                events.Add(new CalendarEvent("RFP #" + rfp.RFPID.ToString() + " Expires", true, rfp.EXPIRES, "#613E82"));
+                events.Add(new CalendarEvent("RFP #" + rfp.RFPID.ToString() + " Expires", true, rfp.EXPIRES, "#9C8AB4"));
             }
 
             serializedEvents = JsonConvert.SerializeObject(events);
