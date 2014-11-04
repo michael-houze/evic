220ae5ef49e8b0f050d06556b94ce9d06172d7a5
ENeises10 <evan.neises@gmail.com>
2014-11-04 16:53:38 -0500

    Vendor Contract changes

--- MVC_DATABASE/Controllers/VendorContractController.cs
+++ MVC_DATABASE/Controllers/VendorContractController.cs
@@ -1,11 +1,24 @@
-﻿using MVC_DATABASE.Models.ViewModels;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.Globalization;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using MVC_DATABASE.Models;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MVC_DATABASE.Models.ViewModels;
 
 
 namespace MVC_DATABASE.Controllers
@@ -28,22 +41,22 @@ namespace MVC_DATABASE.Controllers
 
         //
         //Stores the uploaded form from View VendorContract/Response
-        [HttpPost]
-        public ActionResult Respond(HttpPostedFileBase file)
-        {
-            //Verify a file is selected.
-            if (file != null)
-            {
-                //Extract the file name.
-                var fileName = Path.GetFileName(file.FileName);
-                //Establishes where to save the path using the extracted name.
-                var path = Path.Combine(Server.MapPath(@"~/Content/ContractStore/TestContracts"), fileName);
-                //Saves file.
-                file.SaveAs(path);
-            }
-            //Sends the user back to their respective RFI Index page.
-            return RedirectToAction("Index");
-        }
+        //[HttpPost]
+        //public ActionResult Respond(HttpPostedFileBase file)
+        //{
+        //    //Verify a file is selected.
+        //    if (file != null)
+        //    {
+        //        //Extract the file name.
+        //        var fileName = Path.GetFileName(file.FileName);
+        //        //Establishes where to save the path using the extracted name.
+        //        var path = Path.Combine(Server.MapPath(@"~/Content/ContractStore/TestContracts"), fileName);
+        //        //Saves file.
+        //        file.SaveAs(path);
+        //    }
+        //    //Sends the user back to their respective RFI Index page.
+        //    return RedirectToAction("Index");
+        //}
 
         //public ActionResult Download(int? id, string name)
         //{
@@ -66,6 +79,22 @@ namespace MVC_DATABASE.Controllers
 
         //    throw new ArgumentException("Invalid file name of file not exist");
         //}
+
+        public ActionResult VendorIndex()
+        {
+            EVICEntities db = new EVICEntities();
+            var user = User.Identity.GetUserId();
+
+            List<VendorContract> vendorContract = new List<VendorContract>();
+
+            var vendorContracts = from c in db.CONTRACTs
+                                    join v in db.VENDORs on c.Id equals v.Id
+                                    where c.Id == user
+                                    orderby c.CONTRACTID
+                                    select new VendorContract { contract = c, vendor = v };
+
+            return View(vendorContracts);
+        }
 
         public string Details()
         {
