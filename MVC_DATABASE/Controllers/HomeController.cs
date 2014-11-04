81b260f377e2d9e6a827773d8f6569ad895ae667
Michael Houze <michael.houze@gmail.com>
2014-11-04 09:50:12 -0500

    Login Redirect
    - Logged in user will go to dashboard
    - Logged out users will go to video

--- MVC_DATABASE/Controllers/HomeController.cs
+++ MVC_DATABASE/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MVC_DATABASE.Models;
+using MVC_DATABASE.Models.ViewModels;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,23 @@ namespace MVC_DATABASE.Controllers
 {
     public class HomeController : Controller
     {
+        private EVICEntities db = new EVICEntities();
+
         public ActionResult Index()
         {
-            return View();
+            if (Request.IsAuthenticated)
+            {
+                AdminDashboard model = new AdminDashboard();
+
+                model.pendingVendors = getPendingVendorCount();
+                model.calendarEvents = getCalendarEvents();
+
+                return View(model);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
@@ -25,6 +42,34 @@ namespace MVC_DATABASE.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private int getPendingVendorCount()
+        {
+            var pendingVendors = db.VENDORs.Where(model => model.VENDSTATUS == "PENDING").Count();
+
+            return pendingVendors;
+        }
+
+        private string getCalendarEvents()
+        {
+            string serializedEvents;
+            List<CalendarEvent> events = new List<CalendarEvent>();
+            CalendarEvent tempEvent = new CalendarEvent();
+
+            foreach (var rfi in db.RFIs)
+            {
+                events.Add(new CalendarEvent("RFI #" + rfi.RFIID.ToString() + " Expires", true, rfi.EXPIRES, "#4DB3D0"));
+            }
+
+            foreach (var rfp in db.RFPs)
+            {
+                events.Add(new CalendarEvent("RFP #" + rfp.RFPID.ToString() + " Expires", true, rfp.EXPIRES, "#613E82"));
+            }
+
+            serializedEvents = JsonConvert.SerializeObject(events);
+
+            return serializedEvents;
         }
     }
 }
