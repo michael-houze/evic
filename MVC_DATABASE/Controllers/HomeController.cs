526baf96b0364ac503bcfc3584df48506e57735f
Michael Houze <michael.houze@gmail.com>
2014-11-11 09:24:48 -0500

    Clean up Home Controller
    - Remove placeholder code used before roles were created

--- MVC_DATABASE/Controllers/HomeController.cs
+++ MVC_DATABASE/Controllers/HomeController.cs
@@ -19,24 +19,15 @@ namespace MVC_DATABASE.Controllers
             {
                 if (User.IsInRole("Administrator"))
                 {
-                    AdminDashboard adminModel = new AdminDashboard();
-
-                    adminModel.pendingVendors = getPendingVendorCount();
-                    adminModel.calendarEvents = getCalendarEvents();
-
-                    return RedirectToAction("Admin", "Dashboard", adminModel);
+                    return RedirectToAction("Admin", "Dashboard");
                 }
                 else if (User.IsInRole("Employee"))
                 {
-                    EmployeeDashboard employeeModel = new EmployeeDashboard();
-
-                    return RedirectToAction("Employee", "Dashboard", employeeModel);
+                    return RedirectToAction("Employee", "Dashboard");
                 }
                 else if (User.IsInRole("Vendor"))
                 {
-                    VendorDashboard vendorModel = new VendorDashboard();
-
-                    return RedirectToAction("Vendor", "Dashboard", vendorModel);
+                    return RedirectToAction("Vendor", "Dashboard");
                 }
                 else
                 {
@@ -61,34 +52,6 @@ namespace MVC_DATABASE.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        private int getPendingVendorCount()
-        {
-            var pendingVendors = db.VENDORs.Where(model => model.VENDSTATUS == "PENDING").Count();
-
-            return pendingVendors;
-        }
-
-        private string getCalendarEvents()
-        {
-            string serializedEvents;
-            List<CalendarEvent> events = new List<CalendarEvent>();
-            CalendarEvent tempEvent = new CalendarEvent();
-
-            foreach (var rfi in db.RFIs)
-            {
-                events.Add(new CalendarEvent("RFI #" + rfi.RFIID.ToString() + " Expires", true, rfi.EXPIRES, "#74C0D9"));
-            }
-
-            foreach (var rfp in db.RFPs)
-            {
-                events.Add(new CalendarEvent("RFP #" + rfp.RFPID.ToString() + " Expires", true, rfp.EXPIRES, "#9C8AB4"));
-            }
-
-            serializedEvents = JsonConvert.SerializeObject(events);
-
-            return serializedEvents;
         }
     }
 }
