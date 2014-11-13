4e7ff06d05afc39dd2c4cf8afc87a6997aff3bdf
CodyAlanSanders <cody.alan.sanders@gmail.com>
2014-11-13 09:13:07 -0500

    Added Analytics Part 1
    Added Analytics controller action. Needs to be linked to the Upload
    function. Also, add step to insert into DB.

---
+++ MVC_DATABASE/Models/ReportAnalytics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LinqToExcel.Attributes;
+using Remotion;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_DATABASE.Models
+{
+    public class ReportAnalytics
+    {
+        [Key, Column(Order= 0)]
+        public int RFPID { get; set; }
+
+        [Key, Column(Order= 1)]
+        [ExcelColumn("Commodity Code #")]
+        public int CommodityCode { get; set; }
+
+        [ExcelColumn("New Vendor")]
+        public string ItemVendor { get; set; }
+        
+        [ExcelColumn("Commodity Name")]
+        public string CommodityName { get; set; }
+
+        [ExcelColumn("Current Description")]
+        public string Description { get; set; }
+        
+        [ExcelColumn("Current Price (EA)")]
+        public decimal PreviousPriceEach { get; set; }
+        
+        [ExcelColumn("New EA Price")]
+        public decimal NewPriceEach { get; set; }
+    }
+}
