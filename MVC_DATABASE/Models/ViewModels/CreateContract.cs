f15c5b5f54a53f1f8f42e18374459b795b6a763a
ENeises10 <evan.neises@gmail.com>
2014-11-18 12:26:15 -0500

    Additional Vendor Doc Uploads

---
+++ MVC_DATABASE/Models/ViewModels/CreateContract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.Globalization;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using MVC_DATABASE.Models;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.IO;
+using System.Web.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_DATABASE.Models.ViewModels
+{
+    public class CreateContract
+    {
+        public CONTRACT contract { get; set; }
+
+        [FileExtensions(Extensions = "xlsx,xls,pdf,txt,jpg")]
+        public HttpPostedFileBase file { get; set; }
+    }
+}
